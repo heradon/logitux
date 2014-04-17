49e79bee7728b2392378e4d356386b465041f204
Ralf Matthes <webmaster@rmatthes.de>
2014-04-17 20:57:11 +0200

    several changes

--- gxcolor.sh
+++ gxcolor.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
 #####################
-#        gx19       #
+#      gxcolor      #
 # Ralf Matthes 2014 #
 #####################
 
+# Gerneral variables
 INPUT=/tmp/gxcolor.sh.$$
+#CONFIG=/etc/logitux/gxcolor.cfg
+CONFIG=testfile.cfg
 
 dialog=Xdialog
-title="gx19 Color Chooser"
-backtitle="gx19 Color Chooser"
+title="Logitux G19 Color Chooser"
+backtitle="Logitux G19 Color Chooser"
 R="255"
 G="0"
 B="0"
 i=1
+
+# Read config file
+#echo $(< "$CONFIG" )
+
+#for i in `less -1 $CONFIG | cut -d"=" -f2`; do
+#    echo $i;
+#done
 
 $dialog --backtitle "$backtitle" --title "$title" --colorsel "Choose a Color" 0 0 $R $G $B 2>"${INPUT}"
 
 # cat $INPUT # ONLY FOR DEBUG
 array=( $(< "$INPUT") )
 
-#echo ${array[0]}
-
-
 colors=( red green blue );
 for ((i=0; i<${#array[@]}; i++));
 do
     echo ${array[i]} > /sys/bus/hid/drivers/hid-g19/0003\:046D\:C229.0005/leds/g19_0\:${colors[i]}\:bl/brightness
-#    echo ${array[i]} > /sys/bus/hid/drivers/hid-g19/0003\\:\046D\\:\C229.0005/leds/g19_0\\:${colors[i]}\\:bl/brightness;
+    echo ${array[i]}
 done
